2ec6167027b82b45acfd9a91b1cda54eb2b82498
Dennis Wachira <wachiradenis336@gmail.com>
2025-03-12 09:40:32 +0300

    feat: initial file setup

---
+++ app.js
@@ -0,0 +1,13 @@
+//create arrays
+//Array is a data structure
+//Collection of elements
+//ordered collection that means that each element or member has an assigned index
+
+const shoppingList = [
+    'Bread',
+    'Menengai soap',
+    'sugar',
+    'Quarter',
+    'unga',
+];
+console.log(shoppingList);
